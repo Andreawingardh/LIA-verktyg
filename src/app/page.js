6b77e5dd885306ce37da551fe5ad6827b77debd7
Andreawingardh <andreawingardh@gmail.com>
2025-03-25 14:50:54 +0100

    Added files to allow for use of supabase auth login in form and sign up

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import LoginForm from "./components/form/LoginForm";
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Welcome to our site</h1>
+        <Link href="/login" className={styles.a}>Login</Link>
         <Link href="/company" className={styles.a}>Check out this company</Link>
       </main>
     </div>
