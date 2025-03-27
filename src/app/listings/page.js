f254c4566d2987d9e06aa09f1d4bceb4d4cc3789
Andreawingardh <andreawingardh@gmail.com>
2025-03-27 12:00:52 +0100

    Added session check to Listing page

--- src/app/listings/page.js
+++ src/app/listings/page.js
@@ -1,18 +1,30 @@
 import AuthenticationCheck from "../auth/AuthenticationCheck";
 import CreateListingForm from "../components/form/listing/CreateListingForm";
 import styles from "../page.module.css";
+import { createClient } from "@/utils/supabase/server";
 
+export default async function Listings() {
 
-export default function Listings() {
-    return (
+  const supabase = await createClient()
+  const { data: { user }, error } = await supabase.auth.getUser()
+
+  if (error || !user) {
+    return <p>You must be logged in to create a listing.</p>
+  }
+
+  return <CreateListingForm user={user} />
+  
+  return (
+      
       <div className={styles.page}>
         <main className={styles.main}>
                 <h1>Welcome to Listings</h1>
                 <h2>Hello</h2>
                 <AuthenticationCheck />
-                <CreateListingForm />
+                  <CreateListingForm />
         </main>
       </div>
     );
-  }
+}
+  
   
