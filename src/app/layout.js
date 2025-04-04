2d30b9e6c5df3e7c126940fd4fd8654fe7b6de17
Andreawingardh <andreawingardh@gmail.com>
2025-04-04 15:44:35 +0200

    Changed styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,11 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header/Header.js";
 import Footer from "./components/footer/Footer.js";
-import '@/assets/styles/fonts.css'
-import { Inter } from 'next/font/google';
+import "@/assets/styles/fonts.css";
+import { Inter } from "next/font/google";
+import { styles } from "@/app/page.module.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,21 +19,19 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  organization: 'YRGO',
-  title: "Branschevent",
+  organization: "YRGO",
+  title: "Internify",
   description: "YRGO",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
       <body className={`${inter.variable} ${inter.variable}`}>
         <Header metadata={metadata} />
-        {children}
+        <main>{children}</main>
         <Footer metadata={metadata} />
       </body>
-      
     </html>
   );
 }
