f79f04d850c43c2a80ee9d899d1e1e1d08e6e8db
Andreawingardh <andreawingardh@gmail.com>
2025-04-07 17:51:04 +0200

    cleaned up code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header/Header.js";
 import Footer from "./components/footer/Footer.js";
 import "@/assets/styles/fonts.css";
 import { Inter } from "next/font/google";
-import { styles } from "@/app/page.module.css";
 
 
 const inter = Inter();
