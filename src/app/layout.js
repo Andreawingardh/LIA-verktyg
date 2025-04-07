dbd95f5dca6ee563d4faac6543b9d9c65f663faf
Andreawingardh <andreawingardh@gmail.com>
2025-04-07 17:52:36 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import "@/assets/styles/fonts.css";
 import { Inter } from "next/font/google";
 
 
-const inter = Inter();
+const inter = Inter({ subsets: ["latin"] });
 
 
 export const metadata = {
