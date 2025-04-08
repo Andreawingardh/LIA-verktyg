6eb9841e8450c154d8a5ffa2b5fd0e150c5a57fa
Andreawingardh <andreawingardh@gmail.com>
2025-04-08 11:40:07 +0200

    Renamed company card

--- src/app/components/cards/CompanyCard.js
+++ src/app/components/cards/CompanyCard.js
@@ -1,85 +1,87 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { Status } from "./Status";
 import "./companycard.css";
 import Link from "next/link";
 
 export const CardCompany = ({
   showGreenIcon = true,
-  statusClosed = "Ansökan stängd",
-  status2,
-  showLogotype = true,
-  status = "Matchar dig",
-  location = "Göteborg",
-  company = "Acme Agency",
-  website,
-  id,
-  showApply = true,
-  property1,
+  showLogotype,
+  location,
+  company,
+  showApply,
+  statusProperty,
   className,
   headerClassName,
   applyNowClassName,
+  companyPositions,
+  id,
+  logoUrl,
 }) => {
   return (
-    <div className={`card-company ${property1} ${className}`}>
+    <Link href={`/companies/${id}`} className="card-link">
+      <div className={`card-company ${statusProperty} ${className}`}>
         <div className="content">
-          {showLogotype && <div className="logotype" />}
+          {showLogotype && (
+            <img className="logotype" src={logoUrl} alt={`${company}-logo`} />
+          )}
 
           <div className="text">
-            {["internship-matching", "positions-open"].includes(property1) && (
-              <>
+            {["internship-matching", "positions-open"].includes(
+              statusProperty
+            ) && (
+              <div>
                 <div className={`header ${headerClassName}`}>
-                  <p>{company}</p>
+                  <h5>{company}</h5>
 
                   <p>{location}</p>
                 </div>
 
                 <div className="frame">
-                  {property1 === "internship-matching" && (
-                    <div className="div-wrapper">
-                      <p className="div">Matchar dig</p>
-                    </div>
+                  {statusProperty === "internship-matching" && (
+                    <p className="div">Matchar dig</p>
                   )}
 
-                  {property1 === "positions-open" && (
-                    <Status property1="positions-open" />
+                  {statusProperty === "positions-open" && (
+                    <p>{companyPositions}</p>
                   )}
 
                   {showApply && (
                     <div className={`apply-now ${applyNowClassName}`}>
-                      <p>{status2}</p>
                       {showGreenIcon && <div className="rectangle" />}
                       <p className="text-wrapper-2">Ansök nu</p>
                     </div>
                   )}
                 </div>
-              </>
+              </div>
             )}
 
-            {property1 === "application-closed" && (
+            {statusProperty === "application-closed" && (
               <div className="header">
-                <div className="acme-agency">{company}</div>
+                <div className="company">{company}</div>
 
-                <div className="g-teborg">{location}</div>
+                <div className="location">{location}</div>
               </div>
             )}
           </div>
         </div>
-    </div>
+      </div>
+    </Link>
   );
 };
 
 CardCompany.propTypes = {
   showGreenIcon: PropTypes.bool,
   statusClosed: PropTypes.string,
-  status2: PropTypes.string,
+  companyPositions: PropTypes.string,
   showLogotype: PropTypes.bool,
   status: PropTypes.string,
   website: PropTypes.string,
   location: PropTypes.string,
   company: PropTypes.string,
   showApply: PropTypes.bool,
-  property1: PropTypes.oneOf([
+  logoUrl: PropTypes.string,
+  id: PropTypes.number,
+  statusProperty: PropTypes.oneOf([
     "application-closed",
     "positions-open",
     "internship-matching",
